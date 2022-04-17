8be1a61c84edfd32dcfd7dd17a577f128a66ce20
Selman Kirman <selmankirman@gmail.com>
2022-04-18 01:45:41 +0200

    Add our class level JavaDoc

--- src/C03.java
+++ src/C03.java
@@ -2,5 +2,6 @@ public class C03 {
     public static void main(String[] args) {
         System.out.println("Lokalden Merhaba");
         System.out.println("hahahahahah");
+        System.out.println("dadadadadadadad");
     }
 }
